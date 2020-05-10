869e9d55f91201d56b5d1f83e30f789a67c6fef3
Faiber Alvarado <dev.faiber@gmail.com>
2020-05-10 17:54:25 -0500

    vinculo final de middleware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,9 +33,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        @guest
+
+                        @else
                         <li class="nav-item">
-                        <a class="nav-link" href="{{ route('home')}}">Inicio</a>
+                            <a class="nav-link" href="{{ route('home')}}">Inicio</a>
                         </li>
+                        @endguest
                     </ul>
 
                     <!-- Right Side Of Navbar -->
